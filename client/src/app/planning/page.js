c8dcda85ccfe81ba9f07162aec65862dbeb1aab4
alt012ucsd <alt012@ucsd.edu>
2024-04-11 16:17:31 -0700

    test link

---
+++ client/src/app/planning/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+function page() {
+    return(
+        <Link href="/">
+                <button>Go to Planning Page.</button>
+        </Link>
+    )
+}
+export default page;
