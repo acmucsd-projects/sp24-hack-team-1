39960aff539eca93cf5c524a6e648b6a06e109a6
alaguelo <alaguelo@ucsd.edu>
2024-04-23 16:55:00 -0700

    rough navbar

--- client/src/app/components/navbar/navbar.js
+++ client/src/app/components/navbar/navbar.js
@@ -7,26 +7,30 @@ export default function navbar(){
     return(
         <div id="navbar" className= 'logo'>
             <div className = "title">
-            <Image
-                src="/logo_placeholder.png"
-                alt="placeholder logo"
-                width={150}
-                height={110}
-                priority
-            />
-                <Link href='/'>
-                    Hack Tours
-                </Link>
+                <a href="/" >
+                    <Image
+                    src="/logo.svg"
+                    alt="placeholder logo"
+                    width={150}
+                    height={110}
+                    priority
+                    />
+                </a>
             </div>
+            <div className = "titleHolder">
+                    <Link href='/'>
+                        Hack Tours
+                    </Link>
+                </div>
             <div className = "navButtons">
-                <Link href="/home">
-                    Home
-                </Link>
+                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+                <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Yeseva+One&display=swap" rel="stylesheet"></link>
                 <Link href='/explore'>
                     Explore
                 </Link>
                 <Link href='/Schedule Planner'>
-                    Schedule Planner
+                    Planner
                 </Link>
                 <Link href='/Map'>
                     Map
