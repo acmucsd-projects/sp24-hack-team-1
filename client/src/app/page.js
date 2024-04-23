e4f1ac8921f7661068ac5c395805ce63c58d87a1
Sungjin Seo <lloydseo77@gmail.com>
2024-04-23 16:14:24 -0700

    hi

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+  //hi
   return (
     <main className={styles.main}>
       <div className={styles.description}>
