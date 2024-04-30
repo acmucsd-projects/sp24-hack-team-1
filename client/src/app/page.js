08e7133d7f3071c73892fa1a70f37fb5d7cdb3c9
alaguelo <alaguelo@ucsd.edu>
2024-04-29 17:11:09 -0700

    fixing front page style

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -4,32 +4,41 @@ import NavBar from './components/navbar/navbar'
 
 export default function Home() {
   return (
-    <body>
+    <div className={styles.homeContainer}>
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Yeseva+One&display=swap" rel="stylesheet"></link>
       <div>
       <NavBar></NavBar>
       </div>
-      <h1 className={styles.title}>Travel Hacks in San Diego</h1>
-      <p id='desc'>
-        San Diego is a vibrant coastal city in Southern California known for its 
-        beautiful beaches, perfect weather, and rich cultural diversity.
-      </p>
-      <Image
-        src="/homepic1.svg"
-        alt="homepic1"
-        width={708}
-        height={564}
-        priority
-      />
-      <Image
-        src="/homepic2.svg"
-        alt="homepic2"
-        width={621}
-        height={283}
-        priority
-      />
-      <button>
-        Plan your itinerary
-      </button>
-    </body>
+        <h1 className={styles.title}>Travel Hacks in San Diego</h1>
+        <div className={styles.homebody}>
+          <div className={styles.homeimg1}>
+            <Image className={styles.img1}
+            src="/sdpic1.png"
+            alt="homepic1"
+            width={708}
+            height={564}
+            priority
+            />
+          </div>
+          <div className={styles.homerightside}>
+            <p className={styles.desc}>
+            San Diego is a vibrant coastal city in Southern California known for its 
+            beautiful beaches, perfect weather, and rich cultural diversity.
+            </p>
+            <Image className={styles.img2}
+            src="/img2.jpg"
+            alt="homepic2"
+            width={250}
+            height={250}
+            priority
+            />
+            <button>
+              Plan your itinerary
+            </button>
+          </div>
+      </div>
+    </div>
   )
 }
